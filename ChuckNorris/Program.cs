7e603c46a00e20c67b6a924003e143f5e5f92eca
Kristian Klamas <kklamas@tktk.ee>
2021-11-05 17:36:59 +0200

    New project

---
+++ ChuckNorris/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace ChuckNorris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string randomQuote;
+            randomQuote = GetRandomFromFile("chuck.txt");
+            Console.WriteLine($"{randomQuote}");
+
+            string filePath = @"C:\Users\...\Desktop\Samples\chuck.txt";
+
+            string[] dataFromFile = File.ReadAllLines(filePath);
+        }
+        private static int GenerateRandomIndex(string[] someArray)
+        {
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, someArray.Length);
+            return randomIndex;
+        }
+
+        private static string GetRandomFromFile(string fileName)
+        {
+            string filePath = $@"C:\Users\...\Desktop\Samples\{fileName}";
+            string[] dataFromFile = File.ReadAllLines(filePath);
+            string randomElement = dataFromFile[GenerateRandomIndex(dataFromFile)];
+
+            return randomElement;
+        }
+    }
+}
